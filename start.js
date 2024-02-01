8f76f3cba5626c68f4feb11276a0ffb7b71e8d67
Abhijeet Kumar <116944150+Abhijeet38@users.noreply.github.com>
2024-02-02 02:27:51 +0530

    Create start.js

---
+++ start.js
@@ -0,0 +1,10 @@
+document.getElementById('start-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var player1Name = document.getElementById('player1-name').value;
+    var player2Name = document.getElementById('player2-name').value;
+
+    localStorage.setItem('player1Name', player1Name);
+    localStorage.setItem('player2Name', player2Name);
+
+    window.location.href = 'index.html';
+});
